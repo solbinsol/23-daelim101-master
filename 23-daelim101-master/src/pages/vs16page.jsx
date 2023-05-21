8f2c5a7340127dfa19140de55bc16f80cdc19ce7
solbinsol <qkaejwnj@naver.com>
2023-05-21 21:01:41 +0900

    사진이 나왔다 안나왔다 합니당
    새로고침 뒤지게 하다보면 나와요!

--- 23-daelim101-master/src/pages/vs16page.jsx
+++ 23-daelim101-master/src/pages/vs16page.jsx
@@ -2,112 +2,102 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { ref, listAll, getDownloadURL } from 'firebase/storage';
 import { auth, storage } from '../firebase-config';
-import  '../styles/vs16Man.css'
+import '../styles/vs16Man.css';
+
 function VsPage16() {
   const [displays, setDisplays] = useState([]);
   const [winnerDisplay, setWinnerDisplay] = useState(false);
 
   const [roundCount, setRoundCount] = useState(1);
   const [totalRound, setTotalRound] = useState(8);
-  const [hodus, setHodus] = useState([]);
-  const [winnerHodu, setWinnerHodu] = useState([]);
-useEffect(() => {
-  const listAllImages = async () => {
-    try {
-      const foldersRef = ref(storage, 'user-M');
-      const foldersSnapshot = await listAll(foldersRef);
+  const [participants, setParticipants] = useState([]);
 
-      const randomFolderIndex = Math.floor(Math.random() * foldersSnapshot.prefixes.length);
-      const selectedFolder = foldersSnapshot.prefixes[randomFolderIndex];
+  useEffect(() => {
+    const listAllImages = async () => {
+      try {
+        const foldersRef = ref(storage, 'user-M');
+        const foldersSnapshot = await listAll(foldersRef);
 
-      const imagesRef = ref(storage, selectedFolder);
-      const imagesSnapshot = await listAll(imagesRef);
+        const randomFolderIndex = Math.floor(Math.random() * foldersSnapshot.prefixes.length);
+        const selectedFolder = foldersSnapshot.prefixes[randomFolderIndex];
 
-      if (imagesSnapshot.items.length >= 2) {
-        const randomIndexes = getRandomIndexes(imagesSnapshot.items.length, 2);
-        const imagePromises = randomIndexes.map(async (index) => {
-          const item = imagesSnapshot.items[index];
-          const url = await getDownloadURL(item);
-          return {
-            name: item.name,
-            src: url,
-          };
-        });
+        const imagesRef = ref(storage, selectedFolder);
+        const imagesSnapshot = await listAll(imagesRef);
 
-        const images = await Promise.all(imagePromises);
-        setDisplays(images);
+        if (imagesSnapshot.items.length >= 2) {
+          const randomIndexes = getRandomIndexes(imagesSnapshot.items.length, 2);
+          const imagePromises = randomIndexes.map(async (index) => {
+            const item = imagesSnapshot.items[index];
+            const url = await getDownloadURL(item);
+            return {
+              name: item.name,
+              src: url,
+            };
+          });
+
+          const images = await Promise.all(imagePromises);
+          setDisplays(images);
+          setParticipants(images);
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
+    };
+
+    listAllImages();
+  }, []);
+
+  const getRandomIndexes = (range, count) => {
+    const indexes = new Set();
+    while (indexes.size < count) {
+      const index = Math.floor(Math.random() * range);
+      indexes.add(index);
+    }
+    return Array.from(indexes);
+  };
+
+  const clickEvent = (participant) => () => {
+    const remainingParticipants = participants.filter((p) => p !== participant);
+    const winners = [...participants, participant];
+
+    if (remainingParticipants.length === 1) {
+      setDisplays([remainingParticipants[0]]);
+      setWinnerDisplay(true);
+      setParticipants([remainingParticipants[0]]);
+      setRoundCount(1);
+      setTotalRound(totalRound / 2);
+    } else {
+      setDisplays(remainingParticipants);
+      setParticipants(winners);
+      setRoundCount(roundCount + 1);
     }
   };
 
-  listAllImages();
-}, []);
-
-const getRandomIndexes = (range, count) => {
-  const indexes = new Set();
-  while (indexes.size < count) {
-    const index = Math.floor(Math.random() * range);
-    indexes.add(index);
-  }
-  return Array.from(indexes);
-};
-
-const getUniqueRandomIndexes = (range, count) => {
-  if (range < count) {
-    throw new Error('The range should be greater than or equal to the count.');
-  }
-
-  const indexes = new Set();
-  while (indexes.size < count) {
-    const index = Math.floor(Math.random() * range);
-    indexes.add(index);
-  }
-  return Array.from(indexes);
-};
-
-const clickEvent = hodu => () => {
-    if (hodus.length <= 2) {
-        if (winnerHodu.length === 0) {
-          setDisplays([hodu]);
-          setWinnerDisplay(true);
-        } else {
-          let updatedHodu = [...winnerHodu, hodu];
-          setHodus(updatedHodu);
-          setDisplays([updatedHodu[0], updatedHodu[1]]);
-          setWinnerDisplay([]);
-          setRoundCount(1);
-          setTotalRound(totalRound / 2);
-        }
-    } 
-    else if (hodus.length > 2) {
-        setWinnerDisplay([...winnerHodu, hodu]);
-        setDisplays([hodus[2], hodus[3]]);
-        setHodus(hodus.slice(2));
-        setRoundCount(roundCount + 1);
-    }
-}
+  const handleRestart = () => {
+    setRoundCount(1);
+    setWinnerDisplay(false);
+    setParticipants([]);
+  };
 
   return (
     <div className="page">
       <div className="card">
         {winnerDisplay ? (
-          <div className='WC'>
+          <div className="WC">
             <h1 className="title">최종</h1>
-            {displays.length > 0 && (
+            {participants.length > 0 && (
               <div className="title">
-                <img className="winnerhodu" src={displays[0].src} alt={displays[0].name} />
+                <img className="winnerhodu" src={participants[0].src} alt={participants[0].name} />
               </div>
             )}
-            {displays.length > 0 && (
+            {participants.length > 0 && (
               <div className="title">
-                <label>{displays[0].name}</label>
+                <label>{participants[0].name}</label>
               </div>
             )}
             <div className="action">
               <Link to="/" style={{ textDecoration: 'none' }}>
-                <input type="submit" value="다시하기" />
+                <input type="submit" value="다시하기" onClick={handleRestart} />
               </Link>
               <input type="submit" value="공유하기" />
             </div>
@@ -116,12 +106,12 @@ const clickEvent = hodu => () => {
           <div>
             <h1 className="title">이상형 월드컵 &nbsp;&nbsp;남성&nbsp;&nbsp;{roundCount}/{totalRound}</h1>
             <div className="basic">
-            {displays.map((d) => (
+              {displays.map((d) => (
                 <div className="vsImg" key={d.name} onClick={clickEvent(d)}>
-                <img className="kinghodu" src={d.src} alt={d.name} />
-                <div>{d.name}</div>
-            </div>
-            ))}
+                  <img className="kinghodu" src={d.src} alt={d.name} />
+                  <div>{d.name}</div>
+                </div>
+              ))}
             </div>
           </div>
         )}
@@ -130,4 +120,4 @@ const clickEvent = hodu => () => {
   );
 }
 
-export default VsPage16
+export default VsPage16;
